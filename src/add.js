21f868956492dc8441768e389398baee7613c292
Dameon Green <dameongreen@gmail.com>
2018-03-13 10:19:29 -0400

    First commit with code

---
+++ src/add.js
@@ -0,0 +1,22 @@
+const electron = require('electron')
+const path = require('path')
+const remote = electron.remote
+const ipc = electron.ipcRenderer
+
+const closeBtn = document.getElementById('closeBtn')
+
+closeBtn.addEventListener('click', function(event) {
+    var window = remote.getCurrentWindow();
+    window.close()
+
+})
+
+const updateBtn = document.getElementById('updateBtn')
+
+updateBtn.addEventListener('click', function () {
+  ipc.send('update-notify-value', document.getElementById('notifyVal').value)
+
+  // Close this window
+  var window = remote.getCurrentWindow();
+  window.close();
+})
